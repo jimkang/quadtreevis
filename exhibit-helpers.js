597b9ce4e9e50be06926d6572316e1b2916aab5b
Jim Kang <jimkang@gmail.com>
2014-05-05 09:20:35 -0400

    Updated elWidth and elHeight to approximate more accurate SVG dimensions on Firefox, albeit in a hacky way.

--- exhibit-helpers.js
+++ exhibit-helpers.js
@@ -3,7 +3,13 @@ function createExhibitHelpers() {
     var width = el.clientWidth;
     if (width < 1) {
       // This is necessary on Firefox.
-      width = el.parentElement.clientWidth;
+      width = el.parentElement.clientWidth
+      // SVG width should be the "intrinsic" width.
+      // http://www.w3.org/TR/SVG/struct.html#SVGElementWidthAttribute
+      // On Firefox, it is not, but we can approximate it like this:
+      if (typeof el.width === 'object' && typeof el.width.baseVal === 'object') {
+        width *= el.width.baseVal.value;
+      }
     }
     return width;
   }
@@ -13,6 +19,9 @@ function createExhibitHelpers() {
     if (height < 1) {
       // This is necessary on Firefox.
       height = el.parentElement.clientHeight;
+      if (typeof el.height === 'object' && typeof el.height.baseVal === 'object') {
+        height *= el.height.baseVal.value;
+      }      
     }
     return height;
   }
